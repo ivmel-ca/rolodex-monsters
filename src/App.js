09dd2b5d579e3a3be2491d2645ed8f2a424d20b5
Ivan Melnychenko <ivmel.ca@gmail.com>
2019-10-30 09:47:45 +0200

    set up two dependencies and a homepage in the package json. Added a searchbox.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from "./components/search-box/search-box.component";
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,10 @@ class App extends Component {
 
     this.state = {
       monsters : [],
-    }
+      searchField: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
   }
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`)
@@ -16,10 +20,23 @@ class App extends Component {
         .then(users => this.setState({monsters: users}));
   }
 
+  handleChange(e) {
+    this.setState({'searchField': e.target.value});
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
         <div className="App">
-          <CardList monsters={ this.state.monsters } />
+          <h1>Monsters Rolodex</h1>
+          <SearchBox
+              placeholder={ `search monsters` }
+              handleChange={ this.handleChange }
+          />
+          <CardList monsters={ filteredMonsters } />
         </div>
     )
   }
